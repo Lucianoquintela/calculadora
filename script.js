b1a0d459f1a138d1bc90441221d16eb52f79cd2c
Lucianoquintela0110 <117698113+Lucianoquintela@users.noreply.github.com>
2023-11-23 12:10:21 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 function teclaPressionada() {
    var tecla = document.getElementsByClassName('botoes').value
    console.log(tecla) 
+
+   var tecla1 = tecla
+   document.getElementById('exibirResultado').innerHTML = tecla1
 }
